05ddd3687e174b9839548eb5d12a2c7950b90dd6
Okky Maheswara <okkymahes@gmail.com>
2022-08-11 19:04:05 +0800

    hahaha

--- components/Profile.js
+++ components/Profile.js
@@ -38,11 +38,11 @@ export default function Profile() {
               <h1>
                 {" "}
                 <Typical loop={Infinity} steps={[
-                  'Ethusiatic Dev', 500,
-                  'Full Stack Developer', 500,
-                  'MERN Stack Dev', 500,
-                  'Cross Platform v', 500,
-                  'Nextjs Developer', 500,
+                  'Ethusiatic Dev', 1000,
+                  'Full Stack Developer', 1000,
+                  'MERN Stack Dev', 1000,
+                  'Cross Platform v', 1000,
+                  'Nextjs Developer', 1000,
                 ]} wrapper="p" />
               </h1>
             </span>
